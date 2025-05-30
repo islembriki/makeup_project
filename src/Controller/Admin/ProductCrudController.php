6f45efaa6812d3a9875a9157513f136025bbf62a
Amira el Manaa <amiraelmanaa13@gmail.com>
2025-05-30 23:10:18 +0200

    crud on product

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -4,15 +4,18 @@ namespace App\Controller\Admin;
 
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud; 
+
 use Vich\UploaderBundle\Form\Type\VichImageType;
-use EasyCorp\Bundle\EasyAdminBundle\Field\{
-    IdField, TextField, TextEditorField,
-    MoneyField, IntegerField, AssociationField,
-    ImageField, FormField
-};
+
 class ProductCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
@@ -20,28 +23,45 @@ class ProductCrudController extends AbstractCrudController
         return Product::class;
     }
 
-    public function configureFields(string $pageName): iterable
-    {
-        return [
+   public function configureFields(string $pageName): iterable
+{
+    $fields = [
         IdField::new('id')->hideOnForm(),
         TextField::new('name'),
         TextEditorField::new('description')->hideOnIndex(),
-        MoneyField::new('price')->setCurrency('USD'),
+        MoneyField::new('price')
+            ->setCurrency('USD')
+            ->setStoredAsCents(false),  
         IntegerField::new('stock'),
-        AssociationField::new('category'),
+        AssociationField::new('category')
+            ->autocomplete(),  
+    ];
 
-        // Image configuration
-        ImageField::new('image')
+    if ($pageName === Crud::PAGE_INDEX || $pageName === Crud::PAGE_DETAIL) {
+        $fields[] = ImageField::new('image')
             ->setBasePath('images/products')
-            ->onlyOnIndex(),
-
-        FormField::addPanel('Image Upload'),
-        TextField::new('imageFile')
+            ->setTemplatePath('admin/field/product_image.html.twig');
+    } else {
+        $fields[] = FormField::addPanel('Product Image');
+        $fields[] = TextField::new('imageFile')
             ->setFormType(VichImageType::class)
             ->onlyOnForms()
             ->setFormTypeOptions([
-                'allow_delete' => false,
-            ]),
-    ];
+                'allow_delete' => true,
+                'delete_label' => 'Remove current image',
+            ]);
     }
+
+    return $fields;
+}
+
+    public function configureCrud(Crud $crud): Crud
+{
+    return $crud
+        ->setEntityLabelInSingular('Product')
+        ->setEntityLabelInPlural('Products')
+        ->setPageTitle('index', 'Manage Products')
+        ->setPageTitle('new', 'Add New Product')
+        ->setDefaultSort(['id' => 'DESC']);
+}
 }
