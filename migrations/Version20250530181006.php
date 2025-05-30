c6e3fcb1c92a4041b37625784f59e169a84f8c7a
islembriki <islembriki684@gmail.com>
2025-05-30 19:11:47 +0100

    migrated all the entities

---
+++ migrations/Version20250530181006.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250530181006 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, datetime VARCHAR(255) DEFAULT NULL, status VARCHAR(255) DEFAULT NULL, total DOUBLE PRECISION NOT NULL, INDEX IDX_F5299398A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE TABLE order_item (id INT AUTO_INCREMENT NOT NULL, orderref_id INT NOT NULL, product_id INT NOT NULL, quantity INT DEFAULT NULL, price DOUBLE PRECISION NOT NULL, INDEX IDX_52EA1F092811D9C8 (orderref_id), INDEX IDX_52EA1F094584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE order_item ADD CONSTRAINT FK_52EA1F092811D9C8 FOREIGN KEY (orderref_id) REFERENCES `order` (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE order_item ADD CONSTRAINT FK_52EA1F094584665A FOREIGN KEY (product_id) REFERENCES product (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product CHANGE description description TEXT DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user ADD username VARCHAR(180) NOT NULL, ADD firstname VARCHAR(180) NOT NULL, ADD lastname VARCHAR(180) NOT NULL, ADD address VARCHAR(180) NOT NULL, ADD city VARCHAR(180) NOT NULL, ADD country VARCHAR(180) NOT NULL, ADD phone VARCHAR(180) NOT NULL, ADD postalcode VARCHAR(180) NOT NULL
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE order_item DROP FOREIGN KEY FK_52EA1F092811D9C8
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE order_item DROP FOREIGN KEY FK_52EA1F094584665A
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE `order`
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP TABLE order_item
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE user DROP username, DROP firstname, DROP lastname, DROP address, DROP city, DROP country, DROP phone, DROP postalcode
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE product CHANGE description description VARCHAR(500) DEFAULT NULL
+        SQL);
+    }
+}
