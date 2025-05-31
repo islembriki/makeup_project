11a43fabd1b91cd9098eecdf5519bffec0c264aa
Garbaa Aziza <garbaa0602@gmail.com>
2025-05-31 18:45:47 +0100

    redirection to admin page

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,8 +9,20 @@ use Symfony\Component\Routing\Attribute\Route;
 final class LoginController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
+
     public function index(): Response
     {
+        // Check if user is logged in
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Check if user has ROLE_ADMIN
+        if (in_array('ROLE_ADMIN', $this->getUser()->getRoles())) {
+            return $this->redirectToRoute('admin');
+        }
+
+        // Default for regular users
         return $this->render('product/home.html.twig', [
             'controller_name' => 'LoginController',
         ]);
