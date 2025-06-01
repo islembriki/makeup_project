20a635dcfbdea47cdfaa4b47b731ae9d79150dcd
islembriki <islembriki684@gmail.com>
2025-06-01 12:17:42 +0100

    trying_to_fix_deleteuser

--- migrations/Version20250601111604.php
+++ migrations/Version20250601111604.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250601110233 extends AbstractMigration
+final class Version20250601111604 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,12 +21,6 @@ final class Version20250601110233 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
-            ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE `order` CHANGE user_id user_id INT DEFAULT NULL
-        SQL);
-        $this->addSql(<<<'SQL'
             ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE SET NULL
         SQL);
     }
@@ -36,12 +30,6 @@ final class Version20250601110233 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql(<<<'SQL'
             ALTER TABLE `order` DROP FOREIGN KEY FK_F5299398A76ED395
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE `order` CHANGE user_id user_id INT NOT NULL
-        SQL);
-        $this->addSql(<<<'SQL'
-            ALTER TABLE `order` ADD CONSTRAINT FK_F5299398A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE CASCADE
         SQL);
     }
 }
