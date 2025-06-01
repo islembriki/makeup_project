d19bef9c0e53b2927a0b107ebe605ccca0aaa46b
belkis <belkis.ghdir@insat.ucar.tn>
2025-06-01 01:38:16 +0100

    belkis added the edit my info feature for the user

---
+++ src/Form/UserType.php
@@ -0,0 +1,35 @@
+<?php
+
+//this form is used when the user wants to update his info
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\{TextType, EmailType};
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, ['required' => false])
+            ->add('lastName', TextType::class, ['required' => false])
+            ->add('phone', TextType::class, ['required' => false])
+            ->add('address', TextType::class, ['required' => false])
+            ->add('country', TextType::class, ['required' => false])
+            ->add('city', TextType::class, ['required' => false])
+            ->add('postalcode', TextType::class, ['required' => false]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
+
+
+?>
